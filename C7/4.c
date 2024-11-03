37f54e5a5e86aa3b4bbb7bae71362412833b7b91
User <2085163736@qq.com>
2024-11-03 13:20:37 +0800

    change c7

---
+++ C7/4.c
@@ -0,0 +1,20 @@
+//4.使用if else语句编写一个程序读取输入，读到#停止。用感叹号替换句号，用两个感叹号替换原来的感叹号，最后报告进行了多少次替换。
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100] = {};
+    scanf("%[^#]",str);
+    int len = strlen(str),count = 0;
+    for(int i = 0;i<len;i++)
+    {
+        if(str[i]=='.')str[i]='!',count++;
+        else if(str[i]=='!')
+        {   
+            i++,count++;
+        }
+    }
+    printf("%d\n",count);
+
+    return 0;
+}
